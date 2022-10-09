ea72faa3193433083c251b27adb5a39c9ad06668
vSivarajah <vignesh.sivarajah@digdir.no>
2022-10-09 22:40:24 +0200

    ep08

--- main.go
+++ main.go
@@ -1,8 +1,14 @@
 package main
 
 import (
+	"bytes"
+	"math/rand"
+	"strconv"
 	"time"
 
+	"github.com/sirupsen/logrus"
+	"github.com/vsivarajah/projectx-blockchain/core"
+	"github.com/vsivarajah/projectx-blockchain/crypto"
 	"github.com/vsivarajah/projectx-blockchain/network"
 )
 
@@ -21,7 +27,9 @@ func main() {
 
 	go func() {
 		for {
-			trRemote.SendMessage(trLocal.Addr(), []byte("hello world"))
+			if err := sendTransaction(trRemote, trLocal.Addr()); err != nil {
+				logrus.Error(err)
+			}
 			time.Sleep(1 * time.Second)
 		}
 	}()
@@ -32,4 +40,18 @@ func main() {
 
 	s := network.NewServer(opts)
 	s.Start()
+}
+
+func sendTransaction(tr network.Transport, to network.NetAddr) error {
+	privKey := crypto.GeneratePrivateKey()
+	data := []byte(strconv.FormatInt(int64(rand.Intn(1000000)), 10))
+	tx := core.NewTransaction(data)
+	tx.Sign(privKey)
+	buf := &bytes.Buffer{}
+	if err := tx.Encode(core.NewGobTxEncoder(buf)); err != nil {
+		return err
+	}
+
+	msg := network.NewMessage(network.MessageTypeTx, buf.Bytes())
+	return tr.SendMessage(to, msg.Bytes())
 }
--- network/server.go
+++ network/server.go
@@ -12,6 +12,7 @@ import (
 var defaultBlocktime = 5 * time.Second
 
 type ServerOpts struct {
+	RPCHandler RPCHandler
 	Transports []Transport
 	BlockTime  time.Duration
 	PrivateKey *crypto.PrivateKey
@@ -30,7 +31,8 @@ func NewServer(opts ServerOpts) *Server {
 	if opts.BlockTime == time.Duration(0) {
 		opts.BlockTime = defaultBlocktime
 	}
-	return &Server{
+
+	s := &Server{
 		ServerOpts:  opts,
 		blockTime:   opts.BlockTime,
 		memPool:     NewTxPool(),
@@ -38,6 +40,13 @@ func NewServer(opts ServerOpts) *Server {
 		rpcCh:       make(chan RPC),
 		quitCh:      make(chan struct{}, 1),
 	}
+
+	if opts.RPCHandler == nil {
+		opts.RPCHandler = NewDefaultRPCHandler(s)
+	}
+
+	s.ServerOpts = opts
+	return s
 }
 
 func (s *Server) Start() {
@@ -48,7 +57,9 @@ free:
 	for {
 		select {
 		case rpc := <-s.rpcCh:
-			fmt.Printf("%+v\n", rpc)
+			if err := s.RPCHandler.HandleRPC(rpc); err != nil {
+				logrus.Error(err)
+			}
 		case <-s.quitCh:
 			break free
 
@@ -61,11 +72,7 @@ free:
 	fmt.Println("Server shutdown")
 }
 
-func (s *Server) handleTransaction(tx *core.Transaction) error {
-	if err := tx.Verify(); err != nil {
-		return err
-	}
-
+func (s *Server) ProcessTransaction(from NetAddr, tx *core.Transaction) error {
 	hash := tx.Hash(core.TxHasher{})
 
 	if s.memPool.Has(hash) {
@@ -75,9 +82,15 @@ func (s *Server) handleTransaction(tx *core.Transaction) error {
 
 		return nil
 	}
+	if err := tx.Verify(); err != nil {
+		return err
+	}
+
+	tx.SetFirstSeen(time.Now().UnixNano())
 
 	logrus.WithFields(logrus.Fields{
-		"hash": hash,
+		"hash":           hash,
+		"mempool length": s.memPool.Len(),
 	}).Info("adding new tx to the mempool")
 	return s.memPool.Add(tx)
 }
